616992cb7b3ce646a7cc1b80ac5a921779ca6f41
Jaya <jayamehta29@gmail.com>
2021-03-07 01:48:01 +0530

    file sorting

---
+++ FileSorting(fsmodule)/activity/foldersorting.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+let extensions = require("./util");
+let folderPath = "./Download";
+let extFolderPath;
+
+
+function checkFolder(extension){
+    for(let key in extensions){
+        if(extensions[key].includes(extension)){
+            //string interpolation;
+            extFolderPath = `${folderPath}/${key}`;
+            break;
+        }
+    }
+    return fs.existsSync(extFolderPath);
+}
+
+function moveFile(fileName){
+    //copy file
+    let srcFilePath = `${folderPath}/${fileName}`;
+    let destFilePath = `${extFolderPath}/${fileName}`;
+    fs.copyFileSync(srcFilePath,destFilePath);
+    
+    //delete file
+
+    fs.unlinkSync(srcFilePath);
+
+}
+
+function createFolder() {
+    fs.mkdirSync(extFolderPath);
+  }
+function sortFolder(folderPath){
+    //get content of the folder
+    let content = fs.readdirSync(folderPath);
+    for(let i=0;i<content.length;i++){
+        //get extensions of each file
+        let extensionName = path.extname(content[i]);
+        console.log(extensionName);
+        let extensionFolderExist = checkFolder(extensionName);
+        if(extensionFolderExist){
+            moveFile(content[i]);
+        }else{
+            createFolder();
+            moveFile(content[i]);
+        }
+    }
+}
+sortFolder(folderPath);
+
