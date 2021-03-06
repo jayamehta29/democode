ac55e4e1feeab3341d5089cf3097e34a51ebfa50
Jaya <jayamehta29@gmail.com>
2021-03-06 19:24:09 +0530

    Javascript basics

--- lec0/intro.js
+++ lec0/intro.js
@@ -1,1 +1,27 @@
-console.log("hello world");
+// console.log("hello world");
+
+let obj = {
+    name: "jaya",
+    class : "dev",
+    arr: [
+        10,
+        false,
+        {
+          name: "Steve Rogers",
+          place: "Queens",
+        },
+        "Hey i am a value",
+        [1, 2, 3, 4, 5, 6],
+      ],
+      obj2 : {
+          name : "nsjabck"
+      }
+}
+console.log("accessing object obj");
+
+console.log(obj);
+console.log("traversing inside object obj");
+
+for(let i in obj){
+    console.log(i);
+}
