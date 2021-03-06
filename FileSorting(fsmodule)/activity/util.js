d4c3271178cdc69fbb05bce6fa1ae154af49534f
Jaya <jayamehta29@gmail.com>
2021-03-07 01:49:37 +0530

    extensions file util

---
+++ FileSorting(fsmodule)/activity/util.js
@@ -0,0 +1,8 @@
+let extensions= {
+    "Images": [".png" , ".jpg" , ".jpeg" , ".gif" ],
+    "Audio":  [".mp3"] ,
+    "Documents":[".pdf" , ".txt" , ".doc" ] ,
+    "Compressed":[".zip"] ,
+    "Videos":[".mkv"]
+}
+module.exports=extensions; 
