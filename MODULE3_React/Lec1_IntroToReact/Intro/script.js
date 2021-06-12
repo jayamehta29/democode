a91d51d03124a38e4d6bbd3052f736bee2c4b195
Jaya <jayamehta29@gmail.com>
2021-06-12 22:36:13 +0530

    Intro to react

---
+++ MODULE3_React/Lec1_IntroToReact/Intro/script.js
@@ -0,0 +1,27 @@
+
+//**************** */ (what to render),(where to render)******************************
+
+// ReactDOM.render(
+//     React.createElement("h1", null, "INTRODUCTION TO REACT!!"),
+//     document.querySelector("#root")
+//   );
+
+let names = ["penny" , "sheldon" , "leonard" , "howard" , "rajesh"];
+
+// stateless functional component (sfc)
+function BigHello() {
+  return (
+    <React.Fragment>
+      { names.map( function(name){
+          return <Hello key={name} name={name}></Hello>
+      }) }
+    </React.Fragment>
+  );
+}
+
+function Hello(props) {
+  return <h1>Hello from {props.name} !!</h1>;
+}
+
+// what to render , and where to render
+ReactDOM.render(<BigHello></BigHello>, document.querySelector("#root"));
