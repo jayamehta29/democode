697e3444a8cbb1fe3ff4410e94a80ffcc6e80399
Jaya <jayamehta29@gmail.com>
2021-06-18 17:54:13 +0530

    Todo app add and delete

--- MODULE3_React/todo/src/App.js
+++ MODULE3_React/todo/src/App.js
@@ -1,9 +1,49 @@
-function App() {
-  return (
-    <div className="App">
-      FIRST REACT-Todo_App
-    </div>
-  );
-}
 
+import React, { Component } from 'react';
+import InputBox from "./components/InputBox/InputBox.jsx";
+import TodoList from "./components/TodoList/TodoList.jsx";
+
+class App extends Component {
+  state = { 
+    todos:[
+      {id: "1" , todo : "abcd"},
+      {id: "2" , todo : "qwr"},
+      {id: "3" , todo : "dfg"},
+      {id: "4" , todo : "nbvcxz"},
+    ],
+   };
+
+   deleteTodo =(id)=>{
+     let updatedTodos = this.state.todos.filter(function(todoObj){
+        if(todoObj.id == id){
+          return false;
+        } 
+        return true;
+     });
+     this.setState({
+       todos: updatedTodos,
+     });
+   };
+
+   addTodo = (todo) => {
+    let updatedTodos = [...this.state.todos,{ id: this.state.todos.length + 1, todo: todo },];
+    this.setState({
+      todos:updatedTodos
+    })
+  };
+
+  render() { 
+    let todos = this.state.todos;
+    let deleteTodo = this.deleteTodo;
+    let addTodo = this.addTodo;
+    return ( 
+      <div className="App">
+        <InputBox addTodo = {addTodo}></InputBox>
+        <TodoList todos={todos} deleteTodo = {deleteTodo}></TodoList>
+      </div>
+     );
+  }
+}
+ 
 export default App;
+
---
+++ MODULE3_React/todo/src/components/InputBox/InputBox.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+class InputBox extends Component {
+    state = { 
+        todoValue: "",
+     };
+
+     handleOnChange = (e) => {
+        let value = e.target.value;
+        this.setState({
+            todoValue:value
+        })
+    };
+    
+    handleAddTodo = (e) => {
+        let todo = this.state.todoValue;
+        this.props.addTodo(todo);
+        this.setState({
+            todoValue:""
+        })
+    };
+
+
+    render() { 
+        let todoValue = this.state.todoValue;
+        let handleOnChange = this.handleOnChange;
+        let handleAddTodo = this.handleAddTodo;
+
+        return (
+        <div className="input-box container input-group mt-4">
+            <input
+            type="text"
+            className="form-control"
+            value={todoValue}
+            onChange={handleOnChange}
+            />
+            <button className="btn btn-primary" onClick={handleAddTodo}>
+            Add Todo
+            </button>
+        </div>
+        );
+    }
+}
+ 
+export default InputBox;
