3176a185ab0240acde9c2be0ec829603cd30501b
Jaya <jayamehta29@gmail.com>
2021-07-09 23:00:09 +0530

    hooks demo

---
+++ MODULE3_React/hooks/src/App.js
@@ -0,0 +1,14 @@
+import UseState from "./Components/UseState";
+import UseEffect from "./Components/UseEffect";
+import HookTwo from "./Components/UseEffect";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <UseState></UseState> */}
+      <HookTwo></HookTwo>
+    </div>
+  );
+}
+
+export default App;
